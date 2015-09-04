bedb9c22ead6caed66e3ca148786733b5a236c82
Ben Green <b.green@warwick.ac.uk>
2015-09-04 12:26:02 +0100

    Changes to live charting both channels script.

--- LiveCountsBothChannels.fsx
+++ LiveCountsBothChannels.fsx
@@ -41,8 +41,10 @@ let refreshRate = 20.0
 let windowSize = 200
 
 let mutable mutableString : string = "Both"
+let baseline = 695000
+let baselineString = string(baseline)
 
-let form = new Form(Visible = true, TopMost = true, Width = 600, Height = 600)
+let form = new Form(Text = baselineString, Visible = true, TopMost = true, Width = 600, Height = 600)
 let uiContext = SynchronizationContext.Current
 
 let click0 = new Event<_>()
@@ -68,32 +70,33 @@ clickBoth.Publish.Add(fun _ -> mutableString <- "Both")
 
 form.Controls.Add toolBar  
 
-let showChart channel_0 channel_1 (channel) = async {
+let showChart channel_0 channel_1 (channel) (initialcount: int*int) = async {
     // Charts both channel one and channel two count rates.  
     if channel = Both then
             do! Async.SwitchToContext uiContext // add the chart to the form using the UI thread context
             let chart3 =    
                 Chart.Combine [
                     channel_0
-                    |> Observable.bufferMapiCountOverlapped windowSize (fun i x -> (i, x))
+                    |> Observable.bufferMapiCountOverlapped windowSize (fun i x -> (i, x - (baseline)))
                     |> Observable.sample (TimeSpan.FromMilliseconds refreshRate)
                     |> Observable.observeOnContext uiContext
                     |> LiveChart.FastLine
 
                     channel_1
-                    |> Observable.bufferMapiCountOverlapped windowSize (fun i x -> (i, x))
+                    |> Observable.bufferMapiCountOverlapped windowSize (fun i x -> (i, x - (baseline)))
                     |> Observable.sample (TimeSpan.FromMilliseconds refreshRate)
                     |> Observable.observeOnContext uiContext
                     |> LiveChart.FastLine]
                 |> Chart.WithXAxis(Title = "Time")
                 |> Chart.WithYAxis(Title = "Counts")
+             
             
             new ChartTypes.ChartControl(chart3, Dock = DockStyle.Fill)
             |> form.Controls.Add
         // Charts channel one's count rate. 
         elif channel = Channel1 then
             let chart1 = channel_1
-                         |> Observable.bufferMapiCountOverlapped windowSize (fun i x -> (i, x))
+                         |> Observable.bufferMapiCountOverlapped windowSize (fun i x -> (i, (x - baseline)))
                          |> Observable.sample (TimeSpan.FromMilliseconds refreshRate)
                          |> Observable.observeOnContext uiContext
                          |> LiveChart.FastLine
@@ -105,7 +108,7 @@ let showChart channel_0 channel_1 (channel) = async {
         // Charts channel two's count rate.
         elif channel = Channel0 then
             let chart2 = channel_0
-                         |> Observable.bufferMapiCountOverlapped windowSize (fun i x -> (i, x))
+                         |> Observable.bufferMapiCountOverlapped windowSize (fun i x -> (i, (x - baseline)))
                          |> Observable.sample (TimeSpan.FromMilliseconds refreshRate)
                          |> Observable.observeOnContext uiContext
                          |> LiveChart.FastLine
@@ -118,13 +121,6 @@ let showChart channel_0 channel_1 (channel) = async {
         failwithf "Invalid channel: %A" channel
     do! Async.SwitchToThreadPool()} 
         
-        
-/// Initialise the PicoHarp to histogramming mode, sets bin resolution and overflow limit.
-let initialise = asyncChoice{  
-    let! picoHarp = PicoHarp.Initialise.initialise "1020854" Histogramming 
-    do! Histogram.setBinning picoHarp histogram
-    do! Histogram.stopOverflow picoHarp histogram
-    return picoHarp}
 
 /// Events for charting.
 let channelZeroEvent = new Event<int >()
@@ -134,6 +130,14 @@ let stringtoChannel = function
     | "Channel0" -> Channel0
     | "Channel1" -> Channel1
     | "Both"     -> Both
+    | _ -> failwithf "Not a channel."
+
+
+let channeltoString = function
+    | Channel0 -> "Channe0"
+    | Channel1 -> "Channel"
+    | Both     -> "Both"
+    | _ -> failwithf "Not a channel."
 
 /// Generates chart data. 
 let rec liveCounts (duration:int) (time:int) handle (channel: string) One Two= asyncChoice {
@@ -180,16 +184,37 @@ let rec liveCounts (duration:int) (time:int) handle (channel: string) One Two= a
            do! PicoHarp.Initialise.closeDevice handle 
     }
 
+        
+/// Initialise the PicoHarp to histogramming mode, sets bin resolution and overflow limit.
+let initialise = asyncChoice{  
+    let! picoHarp = PicoHarp.Initialise.initialise "1020854" Histogramming 
+    do! Histogram.setBinning picoHarp histogram
+    do! Histogram.stopOverflow picoHarp histogram
+    return picoHarp}
 
-let experiment duration handle = asyncChoice {
-     // let statments publish both channels events. 
+let rec initial channel = asyncChoice{ 
+   Async.Sleep 2000 |> Async.RunSynchronously 
+   let! initialZeroCount  = PicoHarp.Query.getCountRate handle 0 
+   let! initialFirstCount = PicoHarp.Query.getCountRate handle 0 
+   if initialZeroCount = 0 && channel = Channel0 then
+      Async.Sleep 500 |> Async.RunSynchronously
+      return! initial channel
+   elif initialFirstCount = 0 && channel = Channel1 then
+      Async.Sleep 500 |> Async.RunSynchronously
+      return! initial channel
+   else return (initialZeroCount, initialFirstCount)
+   }     
+
+let experiment duration channel = asyncChoice {
+     let! handle = initialise 
      let publish_0 = channelZeroEvent.Publish
      let publish_1 = channelOneEvent.Publish
-     do! showChart publish_0 publish_1 Both |> AsyncChoice.liftAsync
-     do! liveCounts duration 0 handle "Both" publish_0 publish_1}
+     let! initialCount = initial channel
+     do! showChart publish_0 publish_1 channel initialCount |> AsyncChoice.liftAsync
+     let stringChannel = channeltoString channel
+     do! liveCounts duration 0 handle stringChannel publish_0 publish_1}
 
-initialise |> Async.RunSynchronously
-Async.StartWithContinuations(experiment 10000000 handle , printfn "%A", ignore, ignore)
+Async.StartWithContinuations(experiment 10000000 Both , printfn "%A", ignore, ignore)
 
 
 
